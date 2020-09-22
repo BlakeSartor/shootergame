366cc467b63053f34a6aa566255f664e71fa61e4
Blake Sartor <blakesartor@gmail.com>
2020-09-21 23:52:20 -0500

    adding projectiles

---
+++ Assets/Scripts/PlayerShootProjectiles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootProjectiles : MonoBehaviour
+{
+    public Transform cam;
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject>();
+
+    private GameObject effectToSpawn;
+
+    private void Start()
+    {
+        effectToSpawn = vfx[0];
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SpawnVFX();
+        }
+    }
+
+    void SpawnVFX()
+    {
+        GameObject vfx;
+
+        if (firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+            vfx.transform.localRotation = cam.rotation;
+        }
+        else
+        {
+            Debug.Log("No fire point");
+        }
+    }
+}
---
+++ Assets/Scripts/RotateToMouse.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToMouse : MonoBehaviour
+{
+    public Camera cam;
+    public float maximumLength;
+
+    private Ray mouseRay;
+    private Vector3 pos;
+    private Vector3 dir;
+    private Quaternion rot;
+
+    void Update()
+    {
+        if (cam != null)
+        {
+            RaycastHit hit;
+            var mousePos = Input.mousePosition;
+            mouseRay = cam.ScreenPointToRay(mousePos);
+            if (Physics.Raycast (mouseRay.origin, mouseRay.direction, out hit, maximumLength))
+            {
+                RotateToMouseDirection(gameObject, hit.point);
+            }
+            else
+            {
+                var pos = mouseRay.GetPoint(maximumLength);
+                RotateToMouseDirection(gameObject, pos);
+            }
+
+        }
+        else
+        {
+            Debug.Log("NoCam");
+        }
+    }
+
+    void RotateToMouseDirection(GameObject obj, Vector3 destination)
+    {
+        dir = destination - obj.transform.position;
+        rot = Quaternion.LookRotation(dir);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rot, 1);
+    }
+}
