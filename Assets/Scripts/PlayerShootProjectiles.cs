c2952b7fd2657badb27e75ddb4ae78b713f30307
Blake Sartor <blakesartor@gmail.com>
2020-09-22 23:53:45 -0500

    added projectile selfDestruct()

--- Assets/Scripts/PlayerShootProjectiles.cs
+++ Assets/Scripts/PlayerShootProjectiles.cs
@@ -7,9 +7,7 @@ public class PlayerShootProjectiles : MonoBehaviour
 {
     public Camera fpsCam;
 
-    public float gunDamage = 10f;
     public float gunRange = 100f;
-    public float impactForce = 30f;
 
     public Transform cam;
     public GameObject firePoint;
@@ -42,26 +40,12 @@ public class PlayerShootProjectiles : MonoBehaviour
             {
                 aimPoint = rayHit.point;
                 
-                
-                Target target = rayHit.transform.GetComponent<Target>();
-                if (target != null)
-                {
-                    //target.TakeDamage(gunDamage);
-                }
-
-                if (rayHit.rigidbody != null)
-                {
-                    //rayHit.rigidbody.AddForce(-rayHit.normal * impactForce);
-                }
-                Debug.Log("jere");
             }
             else
             {
                 aimPoint = fpsCam.transform.position + fpsCam.transform.forward * gunRange;
             }
 
-
-            
             vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
             vfx.transform.LookAt(aimPoint);
         }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileMove : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
+    public float damage;
     public float speed;
     public float fireRate;
+    public float impactForce;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SelfDestruct());
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(15f);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
@@ -31,5 +39,18 @@ public class ProjectileMove : MonoBehaviour
     {
         speed = 0;
         Destroy(gameObject);
+        Debug.Log(collision.transform.name);
+
+        Target target = collision.transform.GetComponent<Target>();
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+        }
+
+        if (collision.rigidbody != null)
+        {
+            collision.rigidbody.AddForce(gameObject.transform.forward * impactForce);
+        }
+
     }
 }
